5e414d1e261402f4a3a557660a8c9e46a6374533
David Alves <david.a0595@gmail.com>
2024-08-06 16:57:35 -0300

    Add project files.

---
+++ MessageSimulator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
+
+namespace SignalRChat;
+
+public class MessageSimulator(IHubContext<ChatHub> hubContext) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var random = new Random();
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(random.Next(5000, 10000), stoppingToken);
+
+            var message = $"New message received at {DateTime.Now}";
+            await hubContext.Clients.All.SendAsync("ReceiveNotification", message, cancellationToken: stoppingToken);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using SignalRChat;
+using SignalRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+//builder.Services.AddHostedService<MessageSimulator>();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
